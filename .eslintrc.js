1f17c77e8459f32725ca387b131d777f3f1fd1a5
xuqingling <xql279671304@163.com>
2020-01-09 17:33:14 +0800

    去掉验证

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    // 'vuefix',
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {}
+};
